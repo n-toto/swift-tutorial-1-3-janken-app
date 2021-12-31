700d3d754d124b44e25153023a6018ab741e0ea9
nahotomiki <naho_tech@icloud.com>
2021-12-31 12:20:17 +0900

    JankenApp

--- MyJanken/ViewController.swift
+++ MyJanken/ViewController.swift
@@ -14,6 +14,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var answerImageView: UIImageView!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    var answerNumber = 0
+    
+    @IBAction func shuffleAction(_ sender: Any) {
+        answerNumber = Int.random(in: 0..<3)
+        switch answerNumber {
+        case 0:
+            answerLabel.text = "グー"
+            answerImageView.image = UIImage(named: "gu")
+        case 1:
+            answerLabel.text = "チョキ"
+            answerImageView.image = UIImage(named: "choki")
+        default:
+            answerLabel.text = "パー"
+            answerImageView.image = UIImage(named: "pa")
+        }
+    }
 }
 
